3192ce9b6534b8344fd0d47cf34ba41132e7794a
Siddharth <siddharth.websites@gmail.com>
2024-11-22 18:26:02 +0530

    v1.0.3

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import MyLoadingBar from "./components/MyLoadingBar.jsx";
 import useNewtork from "..//hooks/useNetwork";
 import { useEffect, useState } from "react";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 export default function App() {
   const offline = useNewtork();
   const [start, setStart] = useState(true);
