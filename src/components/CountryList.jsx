075f1faa41e958f6affc3be03390d1954f8c948b
Siddharth <siddharth.websites@gmail.com>
2024-11-29 06:36:25 +0530

    4.0.0

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -15,7 +15,33 @@ export default function CountryList({
 }) {
   const setProgress = useContext(LoadingContext)[1];
   const [noData, setNoData] = useState(true);
- 
+  function allCountries() {
+    let n = 1;
+    return function recall() {
+      if (n == 3) {
+        setLoading(false);
+        setProgress(100);
+        return;
+      }
+      fetch("https://restcountries.com/v3.1/all")
+        .then((res) => {
+          setProgress(30);
+          return res.json();
+        })
+        .then((data) => {
+          setProgress(70);
+          setCountries(data);
+
+          setLoading(false);
+          sessionStorage.setItem("allData", JSON.stringify(data));
+          setProgress(100);
+        })
+        .catch((err) => {
+          n++;
+          recall();
+        });
+    };
+  }
   useEffect(() => {
     setTimeout(() => {
       setNoData(false);
@@ -32,18 +58,7 @@ export default function CountryList({
     } else {
       setLoading(true);
       setProgress(0);
-      fetch("https://restcountries.com/v3.1/all")
-        .then((res) => {
-          setProgress(30);
-          return res.json();
-        })
-        .then((data) => {
-          setProgress(70);
-          setCountries(data);
-          setLoading(false);
-          localStorage.setItem("allData", JSON.stringify(data));
-          setProgress(100);
-        });
+      allCountries()();
     }
     return () => {
       setLoading(true);
